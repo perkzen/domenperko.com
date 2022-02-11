19623161dc331b2d0e4dd356415b254223b5f99b
perkzen <perko.domen@gmail.com>
2022-02-11 19:47:47 +0100

    feat: add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,49 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY);
+  return (
+    <>
+      <Head>
+        <title>Domen Perko</title>
+        <link rel="icon" href="/pd.png" />
+        <meta name="description" content="Portfolio website of Domen Perko." />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="title" content="Domen Perko" />
+        <meta
+          name="keywords"
+          content="domen,perko, Domen Perko, web developer, software developer, personal portfolio, portfolio, react, javascript, typescript, programming"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Domen Perko" />
+        <meta content="Domen Perko | Software Developer" property="og:title" />
+        <meta
+          content="Domen Perko software developer"
+          property="og:description"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}`}
+      />
+      <Script strategy="lazyOnload" id={'google_analytics'}>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,8 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Domen Perko</title>
-        <meta name="description" content="Portfolio website of Domen Perko." />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="title" content="Domen Perko" />
-        <meta
-          name="keywords"
-          content="domen,pekro,Domen Perko,web developer,software developer,personal portfolio,portfolio"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
-        <meta name="author" content="Domen Perko" />
-        <link rel="icon" href="/pd.png" />
-      </Head>
-
       <main>
         <h1>Hello world</h1>
       </main>
