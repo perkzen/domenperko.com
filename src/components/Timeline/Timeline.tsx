f0004545ca6aa82a22779981258e43bdf2c36cc1
perkzen <perko.domen@gmail.com>
2022-06-05 21:31:28 +0200

    feat: add animation

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -3,14 +3,22 @@ import classes from './Timline.module.scss';
 import TimelineItem from '../TimelineItem/TimelineItem';
 import { experiences } from '../../constants/experience';
 import { v4 } from 'uuid';
+import { motion } from 'framer-motion';
+import { floatTransition, floatVariant } from '../../animations/float';
 
 export const Timeline: FC = () => {
   return (
-    <div className={classes.Container}>
-      <h1 className={classes.Title}>Timeline</h1>
+    <motion.div initial="start" animate="end" className={classes.Container}>
+      <motion.h1
+        variants={floatVariant}
+        transition={floatTransition}
+        className={classes.Title}
+      >
+        Timeline
+      </motion.h1>
       {experiences.map((experience) => (
         <TimelineItem key={v4()} experience={experience} />
       ))}
-    </div>
+    </motion.div>
   );
 };
