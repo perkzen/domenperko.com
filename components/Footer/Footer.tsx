a9f43406fe93dd2f8f4a5db3628330ef64f5e49f
perkzen <perko.domen@gmail.com>
2022-06-05 16:59:02 +0200

    feat: add get current year function

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,17 @@ import classes from './Footer.module.scss';
 
 const Footer = () => {
   // get current date
-  return <footer className={classes.Container}>&copy; 2022 Domen Perko</footer>;
+
+  const getCurrentYear = () => {
+    const date = new Date();
+    return date.getFullYear();
+  };
+
+  return (
+    <footer className={classes.Container}>
+      &copy; {getCurrentYear()} Domen Perko
+    </footer>
+  );
 };
 
 export default Footer;
