83e991b27e95217216caa2b37868fe3266bab51e
perkzen <perko.domen@gmail.com>
2022-06-09 12:52:11 +0200

    feat: add about animation

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,28 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import classes from './About.module.scss';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import { floatTransition, floatVariant } from '../../animations/float';
 import Technologies from '../Techonologies/Technologies';
 import Image from 'next/image';
 import image from '../../assets/undraw_feeling_proud_qne1.svg';
+import { useInView } from 'react-intersection-observer';
 
 const About: FC = () => {
+  const [ref, inView] = useInView({ threshold: 0.2 });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0,
+        transition: { duration: 2, type: 'spring', bounce: 0.1 },
+      });
+    }
+  }, [animation, inView]);
+
   return (
     <div className={classes.Container}>
-      <div className={classes.Grid}>
+      <div className={classes.Grid} ref={ref}>
         <Image src={image} alt={'image'} priority />
         <motion.div initial="start" animate="end">
           <motion.h1 variants={floatVariant} transition={floatTransition}>
             About me
           </motion.h1>
-          <p>
+          <motion.p initial={{ x: '200vw' }} animate={animation}>
             Hi, there I&apos;m Domen. I&apos;m a software developer from
             Slovenia. I started programming in 2020, and since then I have built
             a lot of interesting projects, by building them I have added a lot
             of new skills to my repertoire. I have been obsessed with the idea
             of using software to solve practical problems. Software engineering
             is a never ending puzzle that I am passionately engaged in solving.
-          </p>
+          </motion.p>
         </motion.div>
       </div>
       <Technologies />
