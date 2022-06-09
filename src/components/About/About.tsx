a22639c146fe338bad48fb13558502c90af575e3
perkzen <perko.domen@gmail.com>
2022-06-09 13:24:00 +0200

    fix

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -15,32 +15,36 @@ const About: FC = () => {
     if (inView) {
       animation.start({
         x: 0,
-        transition: { duration: 2, type: 'spring', bounce: 0.1 },
+        transition: { duration: 2, type: 'spring', bounce: 0.2 },
       });
     }
   }, [animation, inView]);
 
   return (
     <div className={classes.Container}>
-      <div className={classes.Grid} ref={ref}>
-        <Image src={image} alt={'image'} priority />
-        <motion.div initial="start" animate="end">
-          <motion.h1 variants={floatVariant} transition={floatTransition}>
-            About me
-          </motion.h1>
-          <motion.p initial={{ x: '200vw' }} animate={animation}>
-            Hi, there I&apos;m Domen. I&apos;m a software developer from
-            Slovenia. I started programming in 2020, and since then I have built
-            a lot of interesting projects, by building them I have added a lot
-            of new skills to my repertoire. I have been obsessed with the idea
-            of using software to solve practical problems. Software engineering
-            is a never ending puzzle that I am passionately engaged in solving.
-          </motion.p>
+      <div className={classes.Grid}>
+        <div ref={ref}>
+          <Image src={image} alt={'image'} priority />
+        </div>
+        <motion.div initial={{ x: '200vw' }} animate={animation}>
+          <motion.div initial="start" animate="end">
+            <motion.h1 variants={floatVariant} transition={floatTransition}>
+              About me
+            </motion.h1>
+            <p>
+              Hi, there I&apos;m Domen. I&apos;m a software developer from
+              Slovenia. I started programming in 2020, and since then I have
+              built a lot of interesting projects, by building them I have added
+              a lot of new skills to my repertoire. I have been obsessed with
+              the idea of using software to solve practical problems. Software
+              engineering is a never ending puzzle that I am passionately
+              engaged in solving.
+            </p>
+          </motion.div>
         </motion.div>
       </div>
       <Technologies />
     </div>
   );
 };
-
 export default About;
